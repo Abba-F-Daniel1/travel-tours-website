e508663d73d066b4c38d5786e38587fb82368ea1
Abba.Daniel <abba.fred.daniel@gmail.com>
2024-08-06 23:13:31 +0100

    remove vite-plugin-import-css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import importCss from 'vite-plugin-import-css';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), importCss()],
+  plugins: [react(),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
